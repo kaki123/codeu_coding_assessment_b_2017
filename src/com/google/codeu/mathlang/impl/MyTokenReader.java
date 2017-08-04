90ddc69d712ecd8d37e42f786c70bee0b1be8600
Ka Ki <kakifung@Kakis-MacBook-Pro.local>
2017-08-03 22:28:34 -0700

    readings for different tokens

--- src/com/google/codeu/mathlang/impl/MyTokenReader.java
+++ src/com/google/codeu/mathlang/impl/MyTokenReader.java
@@ -26,10 +26,97 @@ import com.google.codeu.mathlang.parsing.TokenReader;
 // You should not need to change any other files to get your token reader to
 // work with the test of the system.
 public final class MyTokenReader implements TokenReader {
+  
+  private final String source; 
+  private int point = 0;
+
+  private char read() throws IOException {
+    try {
+      if (point < source.length()) {
+        return source.charAt(point++);
+      }
+      else {
+        return '\0';
+      }
+    }
+    catch(Exception exception) {
+      exception.printStackTrace();
+      throw new IOException();
+    }
+  }
+
+  private void back() {
+    point--;
+  }
+
+  private String readName(char a) {
+    String result = "" + a;
+    try {
+      a = read();
+      while (!Character.isWhitespace(a) && Character.isAlphabetic(a) && a != ';' && a != '\0') {
+        result += a;
+        a = read();
+      }
+      if(!Character.isAlphabetic(a)) {
+        back();
+      }
+    }
+    catch(IOException exception) {
+
+    }
+    return result; 
+  }
+
+  private String readString(char a) {
+    String result = "";
+    try {
+      a = read();
+      while (a != '\0' && a != '\"') {
+        result += a;
+        a = read();
+      }
+    }
+    catch(IOException exception) {
+
+    }
+    return result;
+  }
+
+  private double readNumber(char a) {
+    double result = a - '0';
+    boolean decPoint = false;
+    double division = 0.1;
+    try {
+      a = read();
+      while (a != '\0' && ((a >= '0' && a <= '9') || a == '.')) {
+        if(a == '.') {
+          if (decPoint) {
+            break;
+          }
+          decPoint = true;
+        }
+        else if(!foundDot) {
+          result *= 10;
+          result += a - '0';
+        }
+        else {
+          result += (a - '0') * division;
+          division /= 10;
+        }
+        a = read();
+      }
+    }
+    catch (IOException exception) {
+
+    }
+    back();
+    return result;
+  }
 
   public MyTokenReader(String source) {
     // Your token reader will only be given a string for input. The string will
     // contain the whole source (0 or more lines).
+    this.source = source;
   }
 
   @Override
@@ -41,6 +128,36 @@ public final class MyTokenReader implements TokenReader {
     // If for any reason you detect an error in the input, you may throw an IOException
     // which will stop all execution.
 
+    try {
+      char current = read();
+      while (current == ' ' || current == '\n') {
+        current = read();
+      }
+      if (Character.isAlphabetic(current)) {
+        return new NameToken(readName(current));
+      }
+      else if (current >= '0' && current <= '9') {
+        return new NumberToken(readNumber(current));
+      }
+      switch(current) {
+        case '\"':
+          return new StringToken(readString(current));
+        case ';':
+          return new SymbolToken(';');
+        case '=':
+          return new SymbolToken('=');
+        case '+':
+          return new SymbolToken('+');
+        case '-':
+          return new SymbolToken('-');
+        case '\0':
+          return null;
+      }
+    }
+    catch(IOException exception) {
+      exception.printStackTrace();
+      throw new IOException("Can not read next token.");
+    }
     return null;
   }
 }
